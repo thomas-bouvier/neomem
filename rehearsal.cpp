b73d7a8dc3f14b308cbacf45afd9cca868b58173
Thomas Bouvier <contact@thomas-bouvier.io>
2022-04-29 12:23:26 +0200

    fix(cpp): interface

--- rehearsal.cpp
+++ rehearsal.cpp
@@ -7,7 +7,7 @@ namespace py = pybind11;
 PYBIND11_MODULE(rehearsal, m) {
     m.doc() = "pybind11 based streaming rehearsal buffer"; // optional module docstring
     py::class_<stream_loader_t>(m, "StreamLoader")
-        .def(py::init<unsigned int, unsigned int, int64_t>())
+        .def(py::init<unsigned int, unsigned int, unsigned int, int64_t>())
 	.def("accumulate", &stream_loader_t::accumulate)
 	.def("wait", &stream_loader_t::wait);
 }
