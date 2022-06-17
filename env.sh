81b0b7a0d39ff5b917cffbd907ca0a38a4d2721b
Thomas Bouvier <contact@thomas-bouvier.io>
2022-06-17 14:18:18 +0200

    feat: add support for AMP

--- env.sh
+++ env.sh
@@ -5,3 +5,5 @@ export TORCH_USE_RTLD_GLOBAL=YES
 export LD_LIBRARY_PATH=$HOME/.local/lib/python3.10/site-packages/torch/lib
 # Thomas
 #export LD_LIBRARY_PATH=$HOME/.conda/envs/horovod/lib/python3.8/site-packages/torch/lib
+# container on g5k
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/python3.8/dist-packages/torch/
