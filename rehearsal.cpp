02eae8622079d4a0518fa564863cb28629c00712
Thomas Bouvier <contact@thomas-bouvier.io>
2022-08-18 22:45:33 +0200

    deat: support for reconstruction tasks

--- rehearsal.cpp
+++ rehearsal.cpp
@@ -4,6 +4,7 @@
 
 #include "stream_loader.hpp"
 #include "distributed_stream_loader.hpp"
+#include "distributed_stream_loader_recon.hpp"
 
 namespace py = pybind11;
 
@@ -23,4 +24,11 @@ PYBIND11_MODULE(rehearsal, m) {
         .def("wait", &distributed_stream_loader_t::wait, py::call_guard<py::gil_scoped_release>())
         .def("get_rehearsal_size", &distributed_stream_loader_t::get_rehearsal_size)
         .def("get_history_count", &distributed_stream_loader_t::get_history_count);
+
+    py::class_<distributed_stream_loader_recon_t>(m, "DistributedStreamLoaderRecon")
+        .def(py::init<unsigned int, unsigned int, int64_t, uint16_t, std::string, std::vector<std::pair<int, std::string>>&>(), py::call_guard<py::scoped_ostream_redirect>())
+        .def("accumulate", &distributed_stream_loader_recon_t::accumulate, py::call_guard<py::gil_scoped_release>())
+        .def("wait", &distributed_stream_loader_recon_t::wait, py::call_guard<py::gil_scoped_release>())
+        .def("get_rehearsal_size", &distributed_stream_loader_recon_t::get_rehearsal_size)
+        .def("get_history_count", &distributed_stream_loader_recon_t::get_history_count);
 }
--- torch_test.py
+++ torch_test.py
@@ -35,12 +35,18 @@ aug_samples = torch.zeros(B + R, 3, 224, 224)
 aug_labels = torch.randint(high=K, size=(B + R,))
 aug_weights = torch.zeros(B + R)
 
+N_recon = 1000
+
+aug_samples_recon = torch.zeros(B + R, 1, 128, 128)
+aug_targets_recon = torch.zeros(B + R, 1, 128, 128)
+aug_weights_recon = torch.zeros(B + R)
 
 if __name__ == "__main__":
     torch.manual_seed(0)
     random.seed(0)
     np.random.seed(0)
 
+    """
     sl = rehearsal.StreamLoader(
         K, N, C, ctypes.c_int64(torch.random.initial_seed()).value)
 
@@ -67,3 +73,20 @@ if __name__ == "__main__":
             print(f"================================{i} (epoch: {epoch})")
             dsl.accumulate(inputs, target, aug_samples, aug_labels, aug_weights)
             size = dsl.wait()
+    """
+
+    values_recon = np.random.rand(5000, 1, 128, 128)
+    target_recon = np.random.rand(5000, 1, 128, 128)
+    dataset = MyDataset(values_recon, target_recon)
+    loader = DataLoader(dataset=dataset, batch_size=128,
+                        shuffle=True, num_workers=4, pin_memory=True)
+
+    dsl_recon = rehearsal.DistributedStreamLoaderRecon(
+        N_recon, C, ctypes.c_int64(torch.random.initial_seed()).value, 0, "tcp://127.0.0.1:1234", [])
+
+    for epoch in range(10):
+        for i, (inputs, target) in enumerate(loader):
+            #inputs, target = inputs.cuda(), target.cuda()
+            print(f"================================{i} (epoch: {epoch})")
+            dsl_recon.accumulate(inputs, target, aug_samples_recon, aug_targets_recon, aug_weights_recon)
+            size = dsl_recon.wait()
