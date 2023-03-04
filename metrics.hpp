b183eab999c7dc14a89f9d2f6d3d24d7caf5d816
Thomas Bouvier <contact@thomas-bouvier.io>
2023-03-04 10:40:14 +0100

    feat: log accumulate time

--- metrics.hpp
+++ metrics.hpp
@@ -4,14 +4,20 @@
 #include <chrono>
 
 struct metrics_t {
+    std::chrono::system_clock::time_point last_accumulate_time;
+    std::chrono::system_clock::time_point last_queue_time;
+
+    std::chrono::duration<double> accumulate_time;
     std::chrono::duration<double> batch_copy_time;
     std::chrono::duration<double> bulk_prepare_time;
     std::chrono::duration<double> rpcs_resolve_time;
     std::chrono::duration<double> representatives_copy_time;
+    std::chrono::duration<double> queue_time;
     std::chrono::duration<double> buffer_update_time;
 
     std::vector<double> get_durations() const {
         return {
+            accumulate_time.count(),
             batch_copy_time.count(),
             bulk_prepare_time.count(),
             rpcs_resolve_time.count(),
